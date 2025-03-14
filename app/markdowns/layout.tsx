d548928abc7c31d83322e5cf3401f0675a3d2e98
martingdinardi <martingdinardi@gmail.com>
2025-03-14 16:48:39 -0300

    11- README for GitHub and Site added

--- app/markdowns/layout.tsx
+++ app/markdowns/layout.tsx
@@ -1,19 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import ".././globals.css";
 import { Container } from "@/components/ui/Container";
 import MarkdownsNavbar from "./components/MarkdownsNavbar";
 import Footer from "./components/Footer";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "Acme Co.: Building Solutions, Shaping Futures",
@@ -26,18 +14,12 @@ export default async function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        <Container className="flex flex-col justify-between min-h-screen pb-4">
-          <div>
-            <MarkdownsNavbar />
-            {children}
-          </div>
-          <Footer />
-        </Container>
-      </body>
-    </html>
+    <Container className="flex flex-col justify-between min-h-screen pb-4">
+      <div>
+        <MarkdownsNavbar />
+        {children}
+      </div>
+      <Footer />
+    </Container>
   );
 }
